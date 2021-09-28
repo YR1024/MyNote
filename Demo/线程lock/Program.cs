66db21bb6dd83849425e08ba2569a18d8d5a485c
YR1024 <1642963395@qq.com>
2021-09-28 14:55:35 +0800

    线程lock demo

---
+++ Demo/线程lock/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        public static List<int> array = new List<int>();
+        static void Main(string[] args)
+        {
+
+
+            Task t1 = new Task(() =>
+            {
+
+                lock (array)
+                {
+                    while (array.Count<20)
+                    {
+                        array.Add(array.Count);
+                        Console.WriteLine("Add: " + array.Count);
+                        Thread.Sleep(500);
+                    }
+                }
+            });
+            t1.Start();
+
+            Task t2 = new Task(() =>
+            {
+                lock (array)
+                {
+                    while (true)
+                    {
+                        if (array.Count != 0)
+                        {
+                            array.RemoveAt(array.Count - 1);
+                            Console.WriteLine("delete: " + array.Count);
+                        }
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+            t2.Start();
+            Console.ReadLine();
+        }
+
+
+    }
+}
