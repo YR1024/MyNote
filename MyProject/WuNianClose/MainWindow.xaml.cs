fffa6d7157e53a6bf6d8078dc57b59b2eee29254
YR1024 <1642963395@qq.com>
2023-06-27 13:56:45 +0800

    勿念秒杀辅助关闭程序

---
+++ MyProject/WuNianClose/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WuNianClose
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+			(sender as Button).IsEnabled = false;
+			int.TryParse(hour.Text, out int h);
+			int.TryParse(minute.Text, out int m);
+
+			var closeTime = DateTime.Now.Date.AddHours(h).AddMinutes(m);
+			if(DateTime.Now >= closeTime)
+            {
+                closeTime = closeTime.AddDays(1);
+			}
+
+			Task.Run(delegate
+			{
+				try
+				{
+					while (true)
+					{
+						if (DateTime.Now > closeTime)
+						{
+							Process[] qqSpeedProcesses = Process.GetProcessesByName("GameApp");
+							Process[] wunianProcesses = Process.GetProcessesByName("勿念秒杀");
+							if (qqSpeedProcesses.Length >= 1)
+							{
+								foreach (var p in qqSpeedProcesses)
+                                {
+									p.Kill();
+                                }
+								if (wunianProcesses.Length >= 1)
+								{
+									wunianProcesses[0].Kill();
+								}
+								Process.GetCurrentProcess().Kill();
+							}
+						}
+						else
+						{
+							Thread.Sleep(5000);
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			});
+		}
+    }
+}
